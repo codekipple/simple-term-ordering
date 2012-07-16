ccb87f164ab929633f014cf73513bca2d99d5264
Carl Hughes <carl.hughes@wiredmedia.co.uk>
2012-07-16 22:41:28 +0100

    Initial commit.

---
+++ simple-term-ordering.js
@@ -0,0 +1,79 @@
+jQuery('table.widefat tbody th, table.widefat tbody td').css('cursor','move');
+
+jQuery("table.widefat tbody").sortable({
+  items: 'tr:not(.inline-edit-row)',
+  cursor: 'move',
+  axis: 'y',
+  containment: 'table.widefat',
+  scrollSensitivity: 40,
+  helper: function(e, ui) {
+    ui.children().each(function() { jQuery(this).width(jQuery(this).width()); });
+    return ui;
+  },
+  start: function(event, ui) {
+    if ( ! ui.item.hasClass('alternate') ) ui.item.css( 'background-color', '#ffffff' );
+    ui.item.children('td,th').css('border-bottom-width','0');
+    ui.item.css( 'outline', '1px solid #dfdfdf' );
+  },
+  stop: function(event, ui) {
+    ui.item.removeAttr('style');
+    ui.item.children('td,th').css('border-bottom-width','1px');
+  },
+  update: function(event, ui) {
+    jQuery('table.widefat tbody th, table.widefat tbody td').css('cursor','default');
+    jQuery("table.widefat tbody").sortable('disable');
+
+    var ID = ui.item.find('.check-column input').val(); // this is the id
+    var taxonomy = jQuery(this).parents('form').find('input[name$="taxonomy"]').val();
+    var termparent = ui.item.find('.parent').html();   // term parent
+
+    var prevID = ui.item.prev().find('.check-column input').val();
+    var nextID = ui.item.next().find('.check-column input').val();
+
+    // can only sort in same tree
+
+    var prevtermparent = undefined;
+    if ( prevID != undefined ) {
+      var prevtermparent = ui.item.prev().find('.parent').html()
+      if ( prevtermparent != termparent) prevID = undefined;
+    }
+
+    var nexttermparent = undefined;
+    if ( nextID != undefined ) {
+      nexttermparent = ui.item.next().find('.parent').html();
+      if ( nexttermparent != termparent) nextID = undefined;
+    }
+
+    // if previous and next not at same tree level, or next not at same tree level and the previous is the parent of the next, or just moved item beneath its own children
+    if ( ( prevID == undefined && nextID == undefined ) || ( nextID == undefined && nexttermparent == prevID ) || ( nextID != undefined && prevtermparent == ID ) ) {
+      jQuery("table.widefat tbody").sortable('cancel');
+      alert( simple_term_ordering_l10n.RepositionTree );
+      jQuery('table.widefat tbody th, table.widefat tbody td').css('cursor','move');
+      jQuery("table.widefat tbody").sortable('enable');
+      return;
+    }
+
+    // show spinner
+    ui.item.find('.check-column input').hide().after('<img alt="processing" src="images/wpspin_light.gif" class="waiting" style="margin-left: 6px;" />');
+
+    // go do the sorting stuff via ajax
+    jQuery.post( ajaxurl, { action: 'simple_term_ordering', id: ID, previd: prevID, nextid: nextID, taxonomy: taxonomy }, function(response){
+      if ( response == 'children' ) window.location.reload();
+      else {
+        console.log(response);
+        /*var changes = jQuery.parseJSON(response);
+        jQuery.each(changes, function(key,value) { jQuery('#inline_'+key+' .menu_order').html(value); });*/
+        ui.item.find('.check-column input').show().siblings('img').remove();
+        jQuery('table.widefat tbody th, table.widefat tbody td').css('cursor','move');
+        jQuery("table.widefat tbody").sortable('enable');
+      }
+    });
+
+    // fix cell colors
+    jQuery( 'table.widefat tbody tr' ).each(function(){
+      var i = jQuery('table.widefat tbody tr').index(this);
+      if ( i%2 == 0 ) jQuery(this).addClass('alternate');
+      else jQuery(this).removeClass('alternate');
+    });
+  }
+});
---
+++ simple-term-ordering.php
@@ -0,0 +1,174 @@
+<?php
+/**
+ Plugin Name: Simple Term Ordering
+ Plugin URI:
+ Description: Order your terms using drag and drop on the built in term list. For further instructions, open the "Help" tab on the Pages screen. This is a fork of the popular <a href="http://wordpress.org/extend/plugins/simple-page-ordering/">Simple Page Ordering</a> plugin
+ Version: 0.1
+ Author: ralcus
+ Author URI: http://ralcus.com
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+class simple_term_ordering {
+
+  function simple_term_ordering() {
+    // filter terms on the front end
+    add_filter('get_terms_orderby', array( $this, 'frontend_terms_order_filter' ), 10, 2);
+
+    if ( is_admin() ){
+      add_action( 'admin_init', array( $this, 'admin_init' ) );
+      add_action( 'load-edit-tags.php', array( $this, 'wp_edit' ) );
+      add_action( 'wp_ajax_simple_term_ordering', array( $this, 'ajax_simple_term_ordering' ) );
+    }
+
+  }
+
+  function admin_init() {
+    load_plugin_textdomain( 'simple-term-ordering', false, dirname( plugin_basename( __FILE__ ) ) . '/localization/' );
+  }
+
+  function wp_edit() {
+    if ( !current_user_can('edit_others_pages') ) // check permission
+      return;
+
+    add_filter( 'contextual_help', array( $this, 'contextual_help' ) ); // add contextual help to hierarchical post screens
+
+  /*
+   * term filter
+   * ensures terms are ordered by term_order
+   */
+    add_filter('get_terms_orderby', function($orderby, $args){
+      return 't.term_order';
+    }, 10, 2);
+
+    wp_enqueue_script( 'simple-term-ordering', plugin_dir_url( __FILE__ ) . 'simple-term-ordering.js', array('jquery-ui-sortable'), '0.9.7', true );
+    $js_trans = array(
+        'RepositionTree' => __("Items can only be repositioned within their current branch in the page tree / hierarchy (next to pages with the same parent).\n\nIf you want to move this item into a different part of the page tree, use the Quick Edit feature to change the parent before continuing.", 'simple-term-ordering')
+        );
+    wp_localize_script( 'simple-term-ordering', 'simple_term_ordering_l10n', $js_trans );
+
+  }
+
+  function contextual_help( $help )
+  {
+    return $help . '
+      <p><strong>'. __( 'Simple Term Ordering', 'simple_term_ordering' ) . '</strong></p>
+      <p>' . __( 'To reposition an item, simply drag and drop the row by "clicking and holding" it anywhere (outside of the links and form controls) and moving it to its new position.', 'simple-term-ordering' ) . '</p>
+      <p>' . __( 'To keep things relatively simple, the current version only allows you to reposition items within their current tree / hierarchy (next to pages with the same parent). If you want to move an item into or out of a different part of the page tree, use the "quick edit" feature to change the parent.', 'simple-term-ordering' ) . '</p>
+    ';
+  }
+
+  function ajax_simple_term_ordering() {
+    global $wpdb;
+
+    // check permissions again and make sure we have what we need
+    if ( ! current_user_can('edit_others_pages') || empty( $_POST['id'] ) || ( ! isset( $_POST['previd'] ) && ! isset( $_POST['nextid'] ) ) )
+      die(-1);
+
+    // real term?
+    if ( ! $term = get_term( $_POST['id'], $_POST['taxonomy'] ) )
+      die(-1);
+
+    $previd = isset( $_POST['previd'] ) ? $_POST['previd'] : false;
+    $nextid = isset( $_POST['nextid'] ) ? $_POST['nextid'] : false;
+    $new_pos = array(); // store new positions for ajax
+
+    $siblings = get_terms($term->taxonomy, array(
+      'hide_empty' => 0,
+      'hierarchical' => 1,
+      'parent' => $term->parent,
+      'orderby' => 'term_order',
+      'order' => 'ASC',
+      'exclude' => $term->term_id
+    )); // fetch all the siblings (relative ordering)
+
+    $term_order = 0;
+
+    foreach( $siblings as $sibling ) :
+
+      // if this is the term that comes after our repositioned term, set our repositioned term position and increment term order
+      if ( $nextid == $sibling->term_id ) {
+        //update term_order
+        $wpdb->query($wpdb->prepare(
+          "
+            update $wpdb->terms set term_order = '%s'
+            where term_id = '%s'
+          ",
+          $term_order,
+          $term->term_id
+        ));
+        $new_pos[$term->term_id] = $term_order;
+        $term_order++;
+      }
+
+      // if repositioned term has been set, and new items are already in the right order, we can stop
+      if ( isset( $new_pos[$term->term_id] ) && $sibling->term_order >= $term_order ){
+        //break;
+      }
+
+      // set the menu order of the current sibling and increment the menu order
+      $wpdb->query($wpdb->prepare(
+        "
+          update $wpdb->terms set term_order = '%s'
+          where term_id = '%s'
+        ",
+        $term_order,
+        $sibling->term_id
+      ));
+      $new_pos[$sibling->term_id] = $term_order;
+      $term_order++;
+
+
+      // if this is the term that comes before our repositioned term, set our repositioned term position and increment term order
+      if ( ! $nextid && $previd == $sibling->ID ) {
+        $wpdb->query($wpdb->prepare(
+          "
+            update $wpdb->terms set term_order = '%s'
+            where term_id = '%s'
+          ",
+          $term_order,
+          $term->term_id
+        ));
+        $new_pos[$term->term_id] = $term_order;
+        $term_order++;
+      }
+
+
+    endforeach;
+
+    // if the moved term has children, we need to refresh the page
+    $children = get_terms($term->taxonomy, array( 'hide_empty' => 0, 'hierarchical' => 1, 'parent' => $term->term_id ));
+    if ( ! empty( $children ) )
+      die('children');
+
+    die( json_encode($new_pos) );
+  }
+
+  /*
+   * term filter
+   * on the front end if 'term_order' is set as the orderby then it ensures terms are ordered by term_order
+   */
+  function frontend_terms_order_filter($orderby, $args) {
+    if ( $args['orderby'] == 'term_order' ) {
+      $orderby =  't.term_order';
+    }
+
+    return $orderby;
+  }
+
+}
+
+$simple_term_ordering = new simple_term_ordering;
